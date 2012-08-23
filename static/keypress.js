484044c14aebb05fa16f41c2340ff7e5bad0de55
Elissa <welissa@seas.upenn.edu>
2012-08-23 11:10:55 -0400

    .

--- static/keypress.js
+++ static/keypress.js
@@ -4,14 +4,15 @@ $(document).ready(function() {
     
     $(document).keypress(function(e) {
         alert('in keypress');
-        if (e == 32) {
+        var key = e.which;
+        if (key == 32) {
         //space
             if (!entered) { 
                 entered = true;
                 $.get("/bio");
             }
         }
-        if (e == 97) {
+        if (key == 97) {
         // 'a'
             $.get("/bio");
         }
