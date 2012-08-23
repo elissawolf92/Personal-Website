67aea9071377c6b1200709845a00f40304dfa712
Elissa <welissa@seas.upenn.edu>
2012-08-23 11:07:01 -0400

    .

--- static/keypress.js
+++ static/keypress.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
     var entered = false;
+    alert('ready');
     
     $(document).keypress(function(e) {
+        alert('in keypress');
         if (e == 32) {
         //space
             if (!entered) { 
