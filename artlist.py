2b16113395efaf76d74eeabe26f98d2864b88301
Elissa <welissa@seas.upenn.edu>
2012-08-24 22:08:21 -0400

    .

--- artlist.py
+++ artlist.py
@@ -1,9 +1,17 @@
 drawings = {"/art/seatedthumb.png": "/art/seated.png",
 	        "/art/self3thumb.png": "/art/self3.png",
 	        "/art/bodythumb.png": "/art/body.png",
-	        "/art/justinthumb.png": "/art/justin.png"}
+	        "/art/justinthumb.png": "/art/justin.png",
+	        "/art/podiumthumb.png": "/art/podium.png"}
 
 paintings = {"/art/danthumb.png": "/art/dan.png",
-             "/art/tootthumb.png": "/art/toot.png"}
+             "/art/tootthumb.png": "/art/toot.png",
+             "/art/chancetimethumb.png": "/art/chancetime.png",
+             "/art/gourdthumb.png": "art/gourd.png",
+             "/art/perfectionthumb.png": "/art/perfection.png",
+             "/art/racecarthumb.png": "/art/racecar.png"}
              
-digital = {}
+digital = {"/art/armadillothumb.png": "/art/armadillo.png",
+		   "/art/farmthumb.png": "/art/farm.png",
+		   "/art/heartthumb.png": "/art/heart.png",
+		   "/art/metalelissathumb.png": "/art/metalelissa.png"}
