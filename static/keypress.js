1a629f41622dd5f89af5e94f975bcc9b406f7f44
Elissa <welissa@seas.upenn.edu>
2012-08-26 18:52:59 -0400

    .

--- static/keypress.js
+++ static/keypress.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
         }
         else if (key == 122) {
         // z
-        	if (/location/.test(self.location.href) {
+        	if (/location/.test(self.location.href)) {
         		window.location = '/drawing';
         	}
         }
