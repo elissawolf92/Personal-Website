c558503166ed47dd36032536fecfb2b136d12ea0
Elissa <welissa@seas.upenn.edu>
2012-09-05 17:09:16 -0400

    .

--- artlist.py
+++ artlist.py
@@ -5,7 +5,9 @@ drawings = {"/art/seatedthumb.png": "/art/seated.png",
 	        "/art/podiumthumb.png": "/art/podium.png",
 	        "/art/bearthumb.png": "/art/bear.png",
 	        "/art/anonymitythumb.png": "/art/anonymity.png",
-	        "/art/viticulturethumb.png": "/art/viticulture.png"}
+	        "/art/viticulturethumb.png": "/art/viticulture.png",
+	        "/art/femalethumb.png": "/art/female.png",
+	        "/art/smilethumb.png": "/art/smile.png"}
 
 paintings = {"/art/danthumb.png": "/art/dan.png",
              "/art/tootthumb.png": "/art/toot.png",
