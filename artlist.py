c3f1d02bf5e98a291fc857d65e14c6d646efbd52
Elissa <welissa@seas.upenn.edu>
2012-09-05 16:59:42 -0400

    .

--- artlist.py
+++ artlist.py
@@ -2,14 +2,18 @@ drawings = {"/art/seatedthumb.png": "/art/seated.png",
 	        "/art/self3thumb.png": "/art/self3.png",
 	        "/art/bodythumb.png": "/art/body.png",
 	        "/art/justinthumb.png": "/art/justin.png",
-	        "/art/podiumthumb.png": "/art/podium.png"}
+	        "/art/podiumthumb.png": "/art/podium.png",
+	        "/art/bearthumb.png": "/art/bear.png",
+	        "/art/anonymitythumb.png": "/art/anonymity.png",
+	        "/art/viticulturethumb.png": "/art/viticulture.png"}
 
 paintings = {"/art/danthumb.png": "/art/dan.png",
              "/art/tootthumb.png": "/art/toot.png",
              "/art/chancetimethumb.png": "/art/chancetime.png",
              "/art/gourdthumb.png": "art/gourd.png",
              "/art/perfectionthumb.png": "/art/perfection.png",
-             "/art/racecarthumb.png": "/art/racecar.png"}
+             "/art/racecarthumb.png": "/art/racecar.png",
+             "/art/pondthumb.png": "/art/pond.png"}
              
 digital = {"/art/armadillothumb.png": "/art/armadillo.png",
 		   "/art/farmthumb.png": "/art/farm.png",
