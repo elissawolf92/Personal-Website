9b166b95e0ebf9d83ec1ed076475ec6516b22578
Elissa <welissa@seas.upenn.edu>
2012-09-12 22:31:18 -0400

    .

--- main.py
+++ main.py
@@ -17,19 +17,19 @@ def bio():
 
 @app.route('/portfolio')
 def portfolio():
-    return render_template('portfolio_content.html', pieces=artlist.drawings)
+    return render_template('portfolio.html', pieces=artlist.drawings)
 
 @app.route('/portfolio/drawing')
 def drawing():
-    return render_template('portfolio_content.html', pieces=artlist.drawings)
+    return render_template('portfolio.html', pieces=artlist.drawings)
 
 @app.route('/portfolio/painting')
 def painting():
-    return render_template('portfolio_content.html', pieces=artlist.paintings)
+    return render_template('portfolio.html', pieces=artlist.paintings)
 
 @app.route('/portfolio/modeling')
 def modeling():
-    return render_template('portfolio_content.html', pieces=artlist.digital)
+    return render_template('portfolio.html', pieces=artlist.digital)
 
 @app.route('/skills')
 def skills():
